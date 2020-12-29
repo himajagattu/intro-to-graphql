86762ae711e3675c34377c1210f906554f7df2e7
himaja gattu <himajagattu23@gmail.com>
2020-12-28 17:19:00 -0800

    lession-3

--- src/types/product/product.resolvers.js
+++ src/types/product/product.resolvers.js
@@ -9,10 +9,35 @@ const productsTypeMatcher = {
   DRONE: 'Drone'
 }
 
+
 export default {
-  Query: {},
-  Mutation: {},
+  Query: {
+    products(){
+      return Product.find({})
+    },
+    product(_,args,context){
+      console.log("coming to product")
+      return Product.findById(args.id)
+    }
+  },
+  Mutation: {
+    newProduct(_,args,context){
+      return Product.create({...args.input,createdBy:mongoose.Types.ObjectId()})
+    },
+    updateProduct(_,args,context){
+      return Product.findByIdAndUpdate(args.id,args.input,{new:true})
+    },
+    removeProduct(_,args,context){
+      return Product.findByIdAndDelete(args.id)
+    }
+  },
   Product: {
-    __resolveType(product) {}
+    __resolveType(product) {
+      return product
+    },
+      createdBy(product){
+        return User.findById(product.createdBy)
+      }
+    
   }
 }
